bbe6db225bcf628d791e625dd2e36df25682b201
junaide251038 <junaidcreater6@gmail.com>
2025-12-11 14:19:56 +0600

    Create personalinfo.java

---
+++ LabOne/personalinfo.java
@@ -0,0 +1,20 @@
+public class personalinfo {
+
+    public static void main(String[] args) {
+
+        String name = "Muhammad Junaid";
+        String studentID = "E251038";
+        int age = 25;
+        String email = "junaidcreater6@gmail.com";
+        String phone = "8801810597127";
+        String address = "Bohoddarhat, Chattogram, Bangladesh";
+
+        System.out.println("Personal Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
