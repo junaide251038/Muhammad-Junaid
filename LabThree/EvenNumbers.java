e8ef180251aeecca3c4e185909b692d9eb6109c8
junaide251038 <junaidcreater6@gmail.com>
2025-12-11 14:28:45 +0600

    Create EvenNumbers.java

---
+++ LabThree/EvenNumbers.java
@@ -0,0 +1,22 @@
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter the ending number: ");
+        int end = sc.nextInt();
+
+        int[] evenNumbers = new int[(end - start) / 2 + 1];
+        int count = 0;
+
+        int i = start;
+        while (i <= end) {
+            if (i % 2 == 0) {
+                evenNumbers[count] = i;
+                count++;
+            }
+            i++;
+        }
+    
